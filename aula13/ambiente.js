bde370ac3a80ac608c9d4ff24db43539d3cb853d
Gabriel João Kudlawiec <Bielkudlawiec@gmail.com>
2022-01-19 14:18:56 -0300

    Estruturas de Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var b = 1
+do {
+    console.log(`Passo ${b}`)
+    b++
+} while ( b <= 10)
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
